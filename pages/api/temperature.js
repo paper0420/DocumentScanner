d3556df300b54eb1f12eda24ff8de8980eb5af4f
Paper0420 <sumitra0420@gmail.com>
2023-01-20 11:30:53 +0700

    check if temp received

--- pages/api/temperature.js
+++ pages/api/temperature.js
@@ -3,6 +3,7 @@
 export default function handler(req, res) {
   try {
     const { temperature } = req.body;
+    console.log(temperature);
 
     res.status(200).json({ message: "done" });
   } catch (error) {
