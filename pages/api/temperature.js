56708b86e340f95cd55e60a06162ef24f865e5b5
Paper0420 <sumitra0420@gmail.com>
2023-01-20 12:03:52 +0700

    enable CORS

--- pages/api/temperature.js
+++ pages/api/temperature.js
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
+import NextCors from "nextjs-cors";
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   try {
     const { temperature } = req.body;
     console.log(temperature);
